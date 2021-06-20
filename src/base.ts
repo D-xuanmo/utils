724417ab829cf2f3946c98cf5cad4c53c8d442ff
D-xuanmo <me@xuanmo.xin>
2021-06-20 23:52:02 +0800

    feat: 增加判断 url 是否为图片

--- src/base.ts
+++ src/base.ts
@@ -1,20 +1,27 @@
 /**
  * 是否为对象
  * @param {any} source 数据源
- * @returns {boolean} true/false
+ * @returns {boolean}
  */
 export const isObject = (source: any): boolean => Object.prototype.toString.call(source) === '[object Object]'
 
 /**
  * 是否为正则
  * @param {any} source
- * @returns {boolean} true/false
+ * @returns {boolean}
  */
 export const isRegexp = (source: any): boolean => Object.prototype.toString.call(source) === '[object RegExp]'
 
 /**
  * 是否为函数
  * @param {any} source
- * @returns {boolean} true/false
+ * @returns {boolean}
  */
 export const isFunction = (source: any): boolean => Object.prototype.toString.call(source) === '[object Function]'
+
+/**
+ * 判断 url 是否为图片路径
+ * @param {string} url
+ * @returns {boolean}
+ */
+export const isImageUrl = (url: string): boolean => /\.((png)|(jpe?g)|(gif)|(svg)|(webp))$/ig.test(url)
