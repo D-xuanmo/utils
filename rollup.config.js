c212ef657517642f0d67dd23107cca235423af7a
xuanmo <me@xuanmo.xin>
2021-12-04 02:03:32 +0800

    chore: 修改打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,12 @@ import eslint from '@rollup/plugin-eslint'
 import typescript from '@rollup/plugin-typescript'
 import { uglify } from 'rollup-plugin-uglify'
 
+const uglifyOption = {
+  compress: {
+    pure_funcs: ['console.log']
+  }
+}
+
 const baseConfig = {
   input: 'src/index.ts',
 
@@ -38,18 +44,18 @@ export default [
       {
         file: 'dist/javascript-utils.cjs.min.js',
         format: 'cjs',
-        plugins: [uglify()]
+        plugins: [uglify(uglifyOption)]
       },
       {
         file: 'dist/javascript-utils.esm.min.js',
         format: 'es',
-        plugins: [uglify()]
+        plugins: [uglify(uglifyOption)]
       },
       {
         name: 'javascriptUtils',
         file: 'dist/javascript-utils.umd.min.js',
         format: 'umd',
-        plugins: [uglify()]
+        plugins: [uglify(uglifyOption)]
       }
     ]
   }
