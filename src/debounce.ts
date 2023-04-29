290714906e2e1782fb61bf88d9b7974312a4fbaf
D-xuanmo <me@xuanmo.xin>
2023-04-29 12:24:10 +0800

    feat: 增加防抖装饰器版本

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,20 +1,44 @@
 /**
  * 防抖
- * @param {function} fn 执行函数
- * @param {number} wait 等待时间，毫秒
- * @param {boolean} immediate 是否立即执行
+ * @param fn 执行函数
+ * @param wait 等待时间，毫秒
+ * @param immediate 是否立即执行
  */
-export default function debounce(fn: () => void, wait = 0, immediate = false): () => void {
+export function debounce(fn: (...rest: any) => void, wait = 0, immediate = false): () => void {
   let timer: ReturnType<typeof setTimeout>
-  let _immediate = immediate
   return function (...rest: any) {
-    if (_immediate) {
+    if (immediate) {
       fn.apply(this, rest)
-      _immediate = false
+    } else {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        fn.apply(this, rest)
+      }, wait)
     }
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      immediate ? (_immediate = true) : fn.apply(this, rest)
-    }, wait)
+  }
+}
+
+/**
+ * 防抖装饰器版
+ * @param wait 等待时间，毫秒
+ * @param immediate 是否立即执行
+ */
+export function debounceDecorator(wait: number, immediate?: boolean) {
+  let timer: ReturnType<typeof setTimeout>
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const fn = descriptor.value as Function
+    descriptor.value = function handler(...rest: any) {
+      if (immediate) {
+        fn.apply(this, rest)
+      } else {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          fn.apply(this, rest)
+        }, wait)
+      }
+    }
+    return descriptor
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './base'
 export { default as deepCopy } from './deep-copy'
-export { default as debounce } from './debounce'
+export * from './debounce'
 export { default as countDown } from './count-down'
 export * from './URL'
 export { default as ua } from './ua'
